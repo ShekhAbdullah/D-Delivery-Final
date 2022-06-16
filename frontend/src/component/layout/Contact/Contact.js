c00ed2f13726a951d06b988c5f2d4b9f79784c85
ShekhAbdullah <76776291+ShekhAbdullah@users.noreply.github.com>
2022-06-16 16:53:26 +0600

    D-Delivery-Final

---
+++ frontend/src/component/layout/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Contact.css";
+import { Button } from "@material-ui/core";
+
+const Contact = () => {
+  return (
+    <div className="contactContainer">
+      <a className="mailBtn" href="shekh15-2308@diu.edu.bd">
+        <Button>Contact: shekh15-2308@diu.edu.bd</Button>
+      </a>
+    </div>
+  );
+};
+
+export default Contact;
